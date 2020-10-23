7e8736637250e7ee450da33efe7c56c111110bd4
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2020-10-23 23:01:16 +0100

    Update about.js
    Remove image

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,6 @@ export default function About() {
         <div>
             <h1>About me</h1>
             <p>This is where I would talk about who I am</p>
-            <img src="https://source.unsplash.com/random/400x200" alt="" />
         </div>
     );
 }
