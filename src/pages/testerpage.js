bddf417cc3a3d6b9c15822ffe0101a12bba5080e
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2020-10-23 22:52:16 +0100

    Update testerpage.js
    Remove font size styling and added in header, body text and random image

--- src/pages/testerpage.js
+++ src/pages/testerpage.js
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Home() {
-  return <div style={{ color: `purple`, fontSize: `72px` }}>Page test</div>
+    return(
+        <div style={{ color: `purple`}}>
+            <h1>Header</h1>
+            <p>paragraph text</p>
+            <img src="https://source.unsplash.com/random/400x200" alt="" />
+        </div>
+    );
 }
