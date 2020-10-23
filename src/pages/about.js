87f001b247f91b167b416f7c7709f42804db29f4
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2020-10-23 22:59:34 +0100

    Create about.js
    Creating a page for an 'about me' section

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function About() {
+    return(
+        <div>
+            <h1>About me</h1>
+            <p>This is where I would talk about who I am</p>
+            <img src="https://source.unsplash.com/random/400x200" alt="" />
+        </div>
+    );
+}
