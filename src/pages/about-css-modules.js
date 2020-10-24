fff84c6d0d4b43c37d23b6e7ff29d31385a9c97d
Joseph Nino de Guzman <61060613+JosephNinodG@users.noreply.github.com>
2020-10-24 17:28:24 +0100

    Update about-css-modules.js
    Places User component in correct area

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -10,6 +10,15 @@ const User = props => (
       <div className={styles.description}>
         <h2 className={styles.username}>{props.username}</h2>
         <p className={styles.excerpt}>{props.excerpt}</p>
+      </div>
+    </div>
+  )
+
+export default function About() {
+  return (
+    <Container>
+      <h1>About CSS Modules</h1>
+      <p>CSS Modules are cool</p>
         <User
             username="Jane Doe"
             avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
@@ -20,15 +29,6 @@ const User = props => (
             avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
             excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         />
-      </div>
-    </div>
-  )
-
-export default function About() {
-  return (
-    <Container>
-      <h1>About CSS Modules</h1>
-      <p>CSS Modules are cool</p>
     </Container>
   );
 }
